eb096a2837d143a54818aaeb687d79c9ad7b3c9c
kbh <qudgh100456@gmail.com>
2018-05-08 16:43:55 +0900

    RunRPG

---
+++ RunRPG/Assets/New Folder/code/Dynamic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dynamic : MonoBehaviour {
+    public float m_fMoveSpeed;
+    public int m_fRotSpeed;
+    public int maxfMoveSpeed;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        InputProcess();
+        if (m_fMoveSpeed < maxfMoveSpeed && m_fMoveSpeed > 10 ) //입력없는 상태일때 스피드 감소
+        {
+            m_fMoveSpeed -= 0.4f; 
+        }
+     }
+
+    void InputProcess()
+    {
+        float fMoveDist = m_fMoveSpeed * Time.deltaTime;
+        float fRotAngle = m_fRotSpeed * Time.deltaTime;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * fMoveDist);
+                if (m_fMoveSpeed <= maxfMoveSpeed) //w키 누른상태  점차적 스피드 증가
+                    m_fMoveSpeed += 0.8f;
+           
+        }
+    
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * m_fRotSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.up * -m_fRotSpeed);
+        }
+        
+       
+    }
+}
---
+++ RunRPG/Assets/New Folder/code/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public int m_fMoveSpeed=10;
+    public int m_fRotSpeed=10;
+    Player m_cPlayer;
+    // Use this for initialization
+    void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+     
+            InputProcess();
+    }
+    void InputProcess()
+    {
+        float MoveSpeed = Input.GetAxis("Vertical") * m_fMoveSpeed;
+         float RotSpeed = Input.GetAxis("Horizontal") * m_fRotSpeed;
+
+       
+            
+            transform.Translate(0, 0, MoveSpeed);
+        
+        // Rotate around our y-axis
+        transform.Rotate(0, RotSpeed, 0);
+    }
+}
